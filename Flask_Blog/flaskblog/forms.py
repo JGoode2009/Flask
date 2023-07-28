91a55592c397830aafa1e750e6b646275e996a15
JGoode2009 <jonathanedwardgoode@gmail.com>
2023-07-28 00:15:08 -0500

    posting now functional

--- Flask_Blog/flaskblog/forms.py
+++ Flask_Blog/flaskblog/forms.py
@@ -1,7 +1,9 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from flask_wtf.file import FileField, FileAllowed
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from flaskblog.models import User
+from flask_login import current_user
 
 
 # a bit diff from  html forms, flaskform uses python classes  that are then converted into html
@@ -36,3 +38,28 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     remember = BooleanField('Remember me')
     submit = SubmitField('login')
+
+class UpdateAccountForm(FlaskForm):
+    username = StringField('Username', 
+                           validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField("Email",
+                        validators=[DataRequired(), Email() ])
+    picture = FileField('Update Profile Picture', validators=[FileAllowed(['jpg', 'png'])])
+    submit = SubmitField('Update')
+# note that the first argument in the field classes are labels!
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            user= User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError('User name taken, choose another')
+        
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            user = User.query.filter_by(email=email.data).first()
+            if user:
+                raise ValidationError('Email already has an account please login or use different email address')
+
+class PostForm(FlaskForm):
+    title = StringField('Title', validators=[DataRequired()])
+    content=TextAreaField('Content', validators=[DataRequired()])
+    submit = SubmitField('Post')
--- Flask_Blog/flaskblog/routes.py
+++ Flask_Blog/flaskblog/routes.py
@@ -1,34 +1,38 @@
+import os
+import secrets
+from PIL import Image 
 from flask import render_template, url_for, flash, redirect, request, Request
 from flaskblog import app, db, bcrypt   
-from flaskblog.forms import RegistrationForm, LoginForm
+from flaskblog.forms import RegistrationForm, LoginForm, UpdateAccountForm, PostForm
 from flaskblog.models import User, Post
 from flask_login import login_user, current_user, logout_user, login_required
 
 
 
 
-
-example_posts = [
-    {
-        'author': 'JGoode',
-        'title': 'Blog Post 1',
-        'content': "My first blog post :)",
-        'date_posted': 'July 21, 2023'
-    },
-      {
-        'author': 'Eduardo',
-        'title': 'Blog Post 2',
-        'content': "My primera blog post",
-        'date_posted': 'July 21, 2023'
-    }
-]
+# dummy data
+# example_posts = [
+#     {
+#         'author': 'JGoode',
+#         'title': 'Blog Post 1',
+#         'content': "My first blog post :)",
+#         'date_posted': 'July 21, 2023'
+#     },
+#       {
+#         'author': 'Eduardo',
+#         'title': 'Blog Post 2',
+#         'content': "My primera blog post",
+#         'date_posted': 'July 21, 2023'
+#     }
+# ]
 
 
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template("home.html", posts=example_posts)
+    posts = Post.query.all()
+    return render_template("home.html", posts=posts)
 
 @app.route("/about")
 def about():
@@ -78,10 +82,49 @@ def logout():
     logout_user()
     return redirect(url_for('home'))
 
-@app.route("/account")
+def save_picture(form_picture):
+    random_hex = secrets.token_hex(8)
+    _, f_ext = os.path.splitext(form_picture.filename) # to get the file extension
+    picture_fn = random_hex + f_ext
+    picture_path = os.path.join(app.root_path, 'static/profile_pics', picture_fn)
+    output_size = (125, 125)
+    #adding this to resize the profile pics in case they are large
+    i = Image.open(form_picture)
+    i.thumbnail(output_size)
+    i.save(picture_path)
+    return picture_fn
+
+
+@app.route("/account", methods=['GET', 'POST'])
 @login_required
 def account():
+    form = UpdateAccountForm()
+    if form.validate_on_submit():
+            if form.picture.data:
+                picture_file = save_picture(form.picture.data)
+                current_user.image_file = picture_file
+            current_user.username = form.username.data
+            current_user.email = form.email.data
+            db.session.commit()
+            # this appears to be updating the username and email in the account essentially replacing the old account. 
+            flash('Your account has been updated', 'success')
+            return redirect(url_for('account')) # need to redirect for post get redirect pattern (warning about resubmitting data thing)
+    elif request.method == 'GET': #? why just for get? maybe just the form we end up at first?
+        form.username.data = current_user.username
+        form.email.data = current_user.email
     print(" current user image file=", current_user.image_file)
-    image_file = "http://localhost:5000/static/profile_pics/default.jpg"
-    #url_for('static', filename='profile_pics/' + current_user.image_file)
-    return render_template('account.html', title='acount', image_file=image_file)
+    image_file = url_for('static', filename='profile_pics/' + current_user.image_file)
+    return render_template('account.html', title='account', 
+                        image_file=image_file, form=form)
+
+@app.route("/post/new", methods=['GET', 'POST'])
+@login_required
+def new_post():
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Post(title=form.title.data, content=form.content.data, author=current_user)
+        db.session.add(post)
+        db.session.commit()
+        flash('Your post has been created', 'success')
+        return redirect(url_for('home'))
+    return render_template('create_post.html', title='New Post', form=form)
